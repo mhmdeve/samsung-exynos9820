d40a8170f43831c711128e448a788065c746b16d
Master <mhmdeveloperofficial@gmail.com>
2025-03-14 13:54:45 +0500

    nethunter: Improve config and fix compiler error
    drivers/media/v4l2-core/videobuf2-vmalloc.c:434:17: error: incompatible
          pointer types initializing 'void *(*)(struct device *, unsigned
          long, unsigned long, enum dma_data_direction, int)' with an
          expression of type 'void *(struct device *, unsigned long, unsigned
          long, enum dma_data_direction)'
          [-Werror,-Wincompatible-pointer-types]
            .get_userptr    = vb2_vmalloc_get_userptr,
                              ^~~~~~~~~~~~~~~~~~~~~~~
    drivers/media/v4l2-core/videobuf2-vmalloc.c:439:16: error: incompatible
          pointer types initializing 'int (*)(void *, size_t, int)' (aka 'int
          (*)(void *, unsigned long, int)') with an expression of type
          'int (void *)' [-Werror,-Wincompatible-pointer-types]
            .map_dmabuf     = vb2_vmalloc_map_dmabuf,
                              ^~~~~~~~~~~~~~~~~~~~~~
    drivers/media/v4l2-core/videobuf2-vmalloc.c:440:18: error: incompatible
          pointer types initializing 'void (*)(void *, size_t)' (aka 'void
          (*)(void *, unsigned long)') with an expression of type
          'void (void *)' [-Werror,-Wincompatible-pointer-types]
            .unmap_dmabuf   = vb2_vmalloc_unmap_dmabuf,
                              ^~~~~~~~~~~~~~~~~~~~~~~~
    
    Co-authored-by: ravindu644 <Droidcasts@protonmail.com>"
    Signed-off-by: Master <mhmdeveloperofficial@gmail.com>

--- drivers/media/v4l2-core/videobuf2-vmalloc.c
+++ drivers/media/v4l2-core/videobuf2-vmalloc.c
@@ -34,9 +34,9 @@ struct vb2_vmalloc_buf {
 
 static void vb2_vmalloc_put(void *buf_priv);
 
-static void *vb2_vmalloc_alloc(struct device *dev, unsigned long attrs,
-			       unsigned long size, enum dma_data_direction dma_dir,
-			       gfp_t gfp_flags)
+static void *vb2_vmalloc_alloc(struct device *dev, unsigned long size,
+	unsigned long align, enum dma_data_direction dma_dir,
+	gfp_t gfp_flags, int attrs)
 {
 	struct vb2_vmalloc_buf *buf;
 
@@ -71,9 +71,7 @@ static void vb2_vmalloc_put(void *buf_priv)
 	}
 }
 
-static void *vb2_vmalloc_get_userptr(struct device *dev, unsigned long vaddr,
-				     unsigned long size,
-				     enum dma_data_direction dma_dir)
+static void *vb2_vmalloc_get_userptr(struct device *dev, unsigned long vaddr, unsigned long size, enum dma_data_direction dma_dir, int flags)
 {
 	struct vb2_vmalloc_buf *buf;
 	struct frame_vector *vec;
@@ -382,7 +380,7 @@ static struct dma_buf *vb2_vmalloc_get_dmabuf(void *buf_priv, unsigned long flag
 /*       callbacks for DMABUF buffers        */
 /*********************************************/
 
-static int vb2_vmalloc_map_dmabuf(void *mem_priv)
+static int vb2_vmalloc_map_dmabuf(void *mem_priv, size_t size, int flags)
 {
 	struct vb2_vmalloc_buf *buf = mem_priv;
 
@@ -391,7 +389,7 @@ static int vb2_vmalloc_map_dmabuf(void *mem_priv)
 	return buf->vaddr ? 0 : -EFAULT;
 }
 
-static void vb2_vmalloc_unmap_dmabuf(void *mem_priv)
+static void vb2_vmalloc_unmap_dmabuf(void *mem_priv, size_t size)
 {
 	struct vb2_vmalloc_buf *buf = mem_priv;
 
@@ -430,20 +428,20 @@ static void *vb2_vmalloc_attach_dmabuf(struct device *dev, struct dma_buf *dbuf,
 
 
 const struct vb2_mem_ops vb2_vmalloc_memops = {
-	.alloc		= vb2_vmalloc_alloc,
-	.put		= vb2_vmalloc_put,
-	.get_userptr	= vb2_vmalloc_get_userptr,
-	.put_userptr	= vb2_vmalloc_put_userptr,
+    .alloc          = vb2_vmalloc_alloc,
+    .put            = vb2_vmalloc_put,
+    .get_userptr    = vb2_vmalloc_get_userptr,
+    .put_userptr    = vb2_vmalloc_put_userptr,
 #ifdef CONFIG_HAS_DMA
-	.get_dmabuf	= vb2_vmalloc_get_dmabuf,
+    .get_dmabuf     = vb2_vmalloc_get_dmabuf,
 #endif
-	.map_dmabuf	= vb2_vmalloc_map_dmabuf,
-	.unmap_dmabuf	= vb2_vmalloc_unmap_dmabuf,
-	.attach_dmabuf	= vb2_vmalloc_attach_dmabuf,
-	.detach_dmabuf	= vb2_vmalloc_detach_dmabuf,
-	.vaddr		= vb2_vmalloc_vaddr,
-	.mmap		= vb2_vmalloc_mmap,
-	.num_users	= vb2_vmalloc_num_users,
+    .map_dmabuf     = vb2_vmalloc_map_dmabuf,
+    .unmap_dmabuf   = vb2_vmalloc_unmap_dmabuf,
+    .attach_dmabuf  = vb2_vmalloc_attach_dmabuf,
+    .detach_dmabuf  = vb2_vmalloc_detach_dmabuf,
+    .vaddr          = vb2_vmalloc_vaddr,
+    .mmap           = vb2_vmalloc_mmap,
+    .num_users      = vb2_vmalloc_num_users,
 };
 EXPORT_SYMBOL_GPL(vb2_vmalloc_memops);
 
